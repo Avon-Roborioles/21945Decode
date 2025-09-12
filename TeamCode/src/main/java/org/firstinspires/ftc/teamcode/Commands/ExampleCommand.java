a71f35bc6f4441fba36841c233575c1b80e6cc29
FTCRoborioles <aidan.j.yoder@gmail.com>
2025-09-12 11:06:58 -0400

    Imports and empty Example classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ExampleCommand.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+public class ExampleCommand extends CommandBase {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleOpMode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+
+public class ExampleOpMode extends CommandOpMode {
+    @Override
+    public void initialize() {
+
+    }
+}
